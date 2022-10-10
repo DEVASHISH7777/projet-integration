7ed8530e33895c4a629ee56787a4cad10a5ff046
TheGregouze <delannoit.greg@gmail.com>
2022-10-10 12:49:24 +0200

    Bouton test
    Je chipote au app.js pour prendre en main react

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const test = () => {
+    fetch('https://aws.random.cat/meow')
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      console.log(data)
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Ceci est une application pour malentendants!</Text>
+      <Button 
+      onPress={test}
+      title="Test"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button" 
+      /> 
       <StatusBar style="auto" />
     </View>
   );
