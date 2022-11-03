133706aca28bcc7585c36ab5dedbf837a8466e36
Alexandre Doneux <he201962@students.ephec.be>
2022-11-03 21:57:48 +0100

    [FEAT] Ajout d'une communication Bluetooth classic simple entre un téléphone Android et l'Arduino. Les nombres renvoyés sont transmis à travers l'alimentation.
    L'arduino utilise la bibliothèque SoftwareSerial pour créer une seconde
    communication serial autre que celle de l'alimentation. J'utilise
    l'application mobile "Serial Bluetooth Terminal pour envoyer au module
    Bluetooth un nombre. Il est transmis via l'alimentation de l'arduino
    vers l'IDE. Il y a aussi un petit test de valeur du nombre envoyé.
    
    Ce commit est en lien avec l'issue #20

--- main/main.ino
+++ main/main.ino
@@ -1,41 +1,64 @@
-/*
+
 #include <SoftwareSerial.h>
 
 #define rxPin 11 // Broche 11 en tant que RX, à raccorder sur TX du HC-05
 #define txPin 10 // Broche 10 en tant que TX, à raccorder sur RX du HC-05 
-SoftwareSerial mySerial(rxPin, txPin);
+SoftwareSerial hc05(rxPin, txPin);
 
-*/
+#define ledTest 5
 
 
 
 
 void setup()
 {
-Serial.begin(9600); // intitialise la connexion série
-//Serial.begin(38400); // intitialise la connexion série
-//Serial.begin(74880); // intitialise la connexion série
+Serial.begin(9600); // intitialise la connexion série à travers l'alimentation
 
-pinMode(A0,INPUT);
-pinMode(2,OUTPUT);
+//Serial.begin(38400); 
+//Serial.begin(74880); 
+
+hc05.begin(9600); // intitialise la connexion série à travers le hc05
+
+pinMode(A3,INPUT);
+pinMode(ledTest,OUTPUT);
+
+digitalWrite(ledTest, LOW);
 
 
 }
 
 
 void loop() {
-  int readValue = analogRead(A0);
+
+  // Mis en commentaire suite au test 
+  /*
+  int readValue = analogRead(A3);
 
   Serial.println(readValue);
   //delay(5);
 
   if(readValue>310){
-    digitalWrite(2, HIGH);
+    digitalWrite(ledTest, HIGH);
     
   }
   else{
-    digitalWrite(2, LOW);
+    digitalWrite(ledTest, LOW);
 
+  }
+  */
+
+  
+
+  if(hc05.available()){
+    int givenNumber = hc05.read();
+    Serial.write(givenNumber);
+
+    if(givenNumber > 10){
+      digitalWrite(ledTest, HIGH);
+    }
+    else{
+      digitalWrite(ledTest, LOW);
+    }
   }
 
   
